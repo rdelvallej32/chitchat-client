0bbdd98a6c229912aba8a1a8e101cd47d919b7aa
rdelvallej32 <rdelvallej@gmail.com>
2016-06-07 16:30:30 -0400

    Add ajax functions for ratings
    - Add get Ratings function to grab the ratings associated to the user
    - Add create ratings to grab topic id and user id to create a rating

--- assets/scripts/auth/api-topics.js
+++ assets/scripts/auth/api-topics.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const app = require('../app-data');
+const display = require('../display');
 
 const getRandomTopic = (success, failure) => {
   $.ajax({
@@ -27,7 +28,7 @@ const getNytArticle = (success, failure) => {
 };
 
 
-const getRatings = function(success, failure, topicID ){
+const getRatings = function(topicID){
   $.ajax({
     // method: 'GET',
     url: app.server.api + "/ratings/",
@@ -35,7 +36,26 @@ const getRatings = function(success, failure, topicID ){
       Authorization: 'Token token='+ app.currentUser.token,
     },
   })
-  .done(success, topicID)
+  .done(function(data){
+    display.displayRatings(data.ratings, topicID);
+  });
+};
+
+const createRating = (success, failure, score, topic_id) => {
+  $.ajax({
+    method: 'POST',
+    url: app.server.api + '/ratings/',
+    headers: {
+      Authorization: 'Token token=' + app.currentUser.token,
+    },
+    data: {
+      rating: {
+        score: score,
+        user_id: app.currentUser.id,
+        topic_id: topic_id,
+      },
+    },
+  }).done(success)
   .fail(failure);
 };
 
@@ -43,5 +63,6 @@ module.exports = {
   getRandomTopic,
   getNytArticle,
   getRatings,
+  createRating,
   app,
 };
