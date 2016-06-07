52aa6f586f6a7c5510929a681c41d4676eb91926
rdelvallej32 <rdelvallej@gmail.com>
2016-06-07 17:18:22 -0400

    Add function to display ratings
    - function checks to see if the rating is owned by user along with the topic
    and then it sets the id of the rating when the user clicks on a star.

--- assets/scripts/display.js
+++ assets/scripts/display.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const app = require('./app-data.js');
+
 const displayMain = function() {
   let mainTemplate = require('./templates/main.handlebars');
   $('#AskEstherContainer').html(mainTemplate());
@@ -19,8 +21,35 @@ const displayNYTArticle = function(articles) {
   }));
 };
 
+const displayRatings = function(ratings, topicID) {
+  $('#submit-rating, #update-rating').addClass('hidden');
+  let userRating = null;
+  let ratingID = null;
+  let topicRatings = [];
+  for (let i = 0; i < ratings.length; i++) {
+    if (!!app.currentUser && ratings[i].topic.id === topicID && ratings[i].user.email === app.currentUser.email) {
+      userRating = '.star-' + ratings[i].score;
+      ratingID = ratings[i].id;
+    }
+    if (ratings[i].topic.id === topicID) {
+      topicRatings.push(ratings[i].score);
+    }
+  }
+  if(userRating !== null) {
+    $(userRating).prop('checked', true);
+    $('#update-rating').removeClass('hidden');
+    $('.rating').data('id', ratingID);
+  }
+  else {
+    $('.rating').children().prop('checked', false);
+    $('#submit-rating').removeClass('hidden');
+  }
+};
+
 module.exports = {
+  app,
   displayMain,
   displayTopic,
+  displayRatings,
   displayNYTArticle
 };
