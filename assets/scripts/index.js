0e5f2b9381e3aacee1f4b27f8ce72c5905b2ac26
rdelvallej32 <rdelvallej@gmail.com>
2016-06-07 09:43:44 -0400

    Require topic event file
    - Import topic event handlers and have it load on document ready

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,9 +5,11 @@
 
 // use require without a reference to ensure a file is bundled
 const authEvents = require('./auth/auth-events.js');
+const topicEvents = require('./auth/topic-events.js');
 
 $(() => {
   authEvents.authHandlers();
+  topicEvents.topicHandlers();
   $('.sign-out-trigger').hide();
   $('.change-password-trigger').hide();
 });
