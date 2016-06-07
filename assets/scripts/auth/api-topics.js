dab820ecb0206d9bf8c75e4bcce9246e1f7a4d12
rdelvallej32 <rdelvallej@gmail.com>
2016-06-07 14:35:04 -0400

    Begin process of setting up ratings for topics
    - Add radio buttons for star ratings
    - Add api call to get ratings

--- assets/scripts/auth/api-topics.js
+++ assets/scripts/auth/api-topics.js
@@ -26,8 +26,22 @@ const getNytArticle = (success, failure) => {
   .fail(failure);
 };
 
+
+const getRatings = function(success, failure, topicID ){
+  $.ajax({
+    // method: 'GET',
+    url: app.server.api + "/ratings/",
+    headers: {
+      Authorization: 'Token token='+ app.currentUser.token,
+    },
+  })
+  .done(success, topicID)
+  .fail(failure);
+};
+
 module.exports = {
   getRandomTopic,
   getNytArticle,
+  getRatings,
   app,
 };
