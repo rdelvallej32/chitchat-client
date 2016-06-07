5b3f8afadd8b36dc3aa5a7ce4a0c301c4271798b
rdelvallej32 <rdelvallej@gmail.com>
2016-06-07 11:05:45 -0400

    Add ajax call to NYT api
    - Create function that gets a random top story from the NYT api

--- assets/scripts/auth/api-topics.js
+++ assets/scripts/auth/api-topics.js
@@ -14,7 +14,20 @@ const getRandomTopic = (success, failure) => {
   .fail(failure);
 };
 
+const getNytArticle = (success, failure) => {
+  $.ajax({
+    method: "GET",
+    url: app.server.api + '/show-nyt',
+    headers: {
+      Authorization: 'Token token='+ app.currentUser.token,
+    },
+  })
+  .done(success)
+  .fail(failure);
+};
+
 module.exports = {
   getRandomTopic,
+  getNytArticle,
   app,
 };
