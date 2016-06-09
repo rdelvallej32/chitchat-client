7a3daf1c903a729ee7b65773b7b2f29e27591762
rdelvallej32 <rdelvallej@gmail.com>
2016-06-09 18:37:21 -0400

    Change api server to heroku
    - changed api in app data to heroku api

--- assets/scripts/app-data.js
+++ assets/scripts/app-data.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const server = {
-  api: 'http://localhost:3000',
-  // api: 'https://chitchat-api.herokuapp.com',
+  // api: 'http://localhost:3000',
+  api: 'https://chitchat-api.herokuapp.com',
   };
 
   let currentUser = {
